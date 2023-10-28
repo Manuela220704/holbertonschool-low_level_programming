55169f873a5f9930a9b63a8c260423a18eae8093
Manuela220704 <manumbaltan@gmail.com>
2023-10-27 23:16:26 -0700

    correcion 2

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+ *  * main - Prints "Programming is like building a multilingual puzzle", followed by a new line.
+ *   *
+ *    * Return: Always 0.
+ *     */
 int main(void)
 {
-	    puts("Programming is like building a multilingual puzzle");
+	    puts("Programming is like building a multilingual puzzle"); /* Use the function puts */
 
 	        return (0);
 }
