e77972965c2dafa1494fd378390cc020a62b849c
Manuela220704 <manumbaltan@gmail.com>
2023-10-27 20:49:32 -0700

    todos

---
+++ hello_world/4-puts.c
@@ -0,0 +1,19 @@
+/*
+ *  *File: 4-puts.c
+ *   *Author: Brayan Steven Salazar Perdomo
+ *    */
+
+
+#include <stdio.h>
+
+/**
+ *  * main - C program: Prints "Programming is like building a multilingual puzzle
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
---
+++ hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ *  * File: 5-printf.c
+ *   * Author: Brayan Steven Salazar
+ *    */
+
+
+#include <stdio.h>
+
+
+/**
+ *  * main - C program: prints a specific line of text
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
