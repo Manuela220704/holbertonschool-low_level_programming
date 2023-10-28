72f5ccc58c17181c7c877b7d53b3e3420a19c09f
Manuela220704 <manumbaltan@gmail.com>
2023-10-27 23:35:10 -0700

    correcion 2

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,19 +1,19 @@
-/*
- *  * File: 4-puts.c
- *   * Auth: Brennan D Baraban <375@holbertonschool.com>
- *    */
+*
+*  *File: 4-puts.c
+*   *Author: manuelao
+*    */
+
 
 #include <stdio.h>
 
 /**
- *  * main - Prints "Programming is like building a multilingual
- *   *                puzzle"
- *    *
- *     * Return: Always 0.
- *      */
+ *  * main - C program: Prints "Programming is like building a multilingual puzzle
+ *   *
+ *    * Return: Always 0.
+ *     */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle\");
 
+		puts("\"Programming is like building a multilingual puzzle\");
 			return (0);
 }
