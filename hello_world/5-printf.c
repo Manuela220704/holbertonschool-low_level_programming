de5a33175e281b50fa06e1ebfd37913a208b6c40
Manuela220704 <manumbaltan@gmail.com>
2023-10-29 18:49:51 -0700

    5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,20 @@
+/*
+ * File: 5-printf.c
+ *  Author: Manueka Marin Baltan
+ */
+
+
+#include <stdio.h>
+
+
+/**
+ *  main - C program: prints a specific line of text
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
