3d91accf39d49887691a7305462ab76fb78667e9
Manuela220704 <manumbaltan@gmail.com>
2023-11-01 08:14:04 -0700

    0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,12 +9,13 @@
 
 /**
  * main - Entry point
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
