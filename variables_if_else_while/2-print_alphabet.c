3d93dfe6a5819f73bf20f60c727956de328c5dbc
Manuela220704 <manumbaltan@gmail.com>
2023-11-02 08:32:30 -0700

    2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - using a looping for alphabet
+ * Return: return (0).
+ */
+int main(void)
+{
+	char alphabet;
+
+	alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
