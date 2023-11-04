77ca83583ab84a7e5146f3c50607be2f82d8197a
Manuela220704 <manumbaltan@gmail.com>
2023-11-03 21:15:32 -0700

    acabado+

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *add - Function that adds two integers and returns the result
+ *@n1: Integer variable declaration
+ *@n2: Integer variable declaration
+ *Return: Result of the sum of n1 and n2
+       */
+int add(int n1, int n2)
+{
+	return ((n1 + n2));
+}
