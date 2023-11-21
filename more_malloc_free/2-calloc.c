71ed1ead8719c60251faf3b819a1d9216dbf1b64
Manuela220704 <manumbaltan@gmail.com>
2023-11-20 20:32:28 -0800

    ejercicio2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,34 +1,29 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-
 /**
- *_calloc - Allocates memory for an array of nmemb elements of size bytes each,
- *           and initializes all the memory to zero.
- *@nmemb: Number of elements in the array.
- *@size: Size of each element in bytes.
+ * _calloc - allocates memory for an array.
+ * @nmemb: number of elements.
+ * @size: size of bytes.
  *
- *Return: A pointer to the allocated memory. If nmemb or size is 0,
- *        or if the allocation fails, returns NULL.
+ * Return: pointer to the allocated memory.
+ * if nmemb or size is 0, returns NULL.
+ * if malloc fails, returns NULL.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	char *p;
 	unsigned int i;
-	unsigned int num_size_elements;
-
-	void *asignMemory;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	asignMemory = malloc(nmemb * size);
+	p = malloc(nmemb * size);
 
-	if (asignMemory == NULL)
+	if (p == NULL)
 		return (NULL);
 
-	num_size_elements = (nmemb * size);
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
 
-	for (i = 0; i < num_size_elements; i++)
-		*((char *)asignMemory + i) = 0;
-
-	return (asignMemory);
+	return (p);
 }
